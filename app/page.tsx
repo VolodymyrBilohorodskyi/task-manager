0af2984db65a0772e26c875b6cc46f86827463f5
snowEnix <alexey.svidrov@gmail.com>
2023-06-14 12:37:13 +0300

    FixE

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
-import Tasks from '@/components/tasks/Tasks'
-import Sidebar from '@/components/sidebar/Sidebar'
-import { AddFolderType } from '@/types'
-import { GetFolderFetch } from '@/services/FolderAPI'
+import Tasks from "@/components/tasks/Tasks";
+import Sidebar from "@/components/sidebar/Sidebar";
+import { AddFolderType } from "@/types";
+import { GetFolderFetch } from "@/services/FolderAPI";
 
 const Home = async () => {
-  const folderData: AddFolderType[] = await GetFolderFetch()
+  const folderData: AddFolderType[] = await GetFolderFetch();
+
   return (
-    <div className='container'>
-      <div className='wrapper'>
+    <div className="container">
+      <div className="wrapper">
         <Sidebar folders={folderData} />
         <Tasks />
       </div>
     </div>
-  )
-}
-export default Home
+  );
+};
+export default Home;
